a37f842cea0bd5ab4f8fea5c1c7d0b6fab7e00dd
James Root <jamesroot@gmail.com>
2014-10-20 16:18:28 -0400

    Updated StaticLoggerBinder to correctly name field

--- src/main/java/org/slf4j/impl/StaticLoggerBinder.java
+++ src/main/java/org/slf4j/impl/StaticLoggerBinder.java
@@ -1,5 +1,6 @@
 package org.slf4j.impl;
 
+import android.util.Log;
 import edu.cmu.sei.ams.log.android.LoggerFactory;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.spi.LoggerFactoryBinder;
@@ -12,6 +13,7 @@ import org.slf4j.spi.LoggerFactoryBinder;
 public class StaticLoggerBinder implements LoggerFactoryBinder
 {
     private static final StaticLoggerBinder SINGLETON = new StaticLoggerBinder();
+    public static String REQUESTED_API_VERSION = "1.7.7";
 
     private final String loggerFactoryClsStr = LoggerFactory.class.getName();
     private final ILoggerFactory loggerFactory = new LoggerFactory();
@@ -24,6 +26,7 @@ public class StaticLoggerBinder implements LoggerFactoryBinder
     @Override
     public ILoggerFactory getLoggerFactory()
     {
+        Log.v("LOGGER", "Returning the logger factory");
         return loggerFactory;
     }
 
